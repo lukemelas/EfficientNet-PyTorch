b15d773699137965fc124428240f489a5f8eba56
Luke <luke.melas@gmail.com>
2019-06-01 01:14:11 -0700

    Continued restructuring project

---
+++ efficientnet_pytorch/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.0"
+from .model import EfficientNet
+from .utils import (
+    GlobalParams,
+    BlockArgs,
+    BlockDecoder,
+    efficientnet,
+    get_model_params,
+)
+
--- efficientnet_pytorch/model.py
+++ efficientnet_pytorch/model.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-from utils import (
+from .utils import (
     relu_fn,
     round_filters,
     round_repeats,
