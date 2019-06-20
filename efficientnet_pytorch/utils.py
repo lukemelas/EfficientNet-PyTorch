939d4abdeefc07e63d8bd42e7223365a4bc67942
Luke Melas-Kyriazi <lukemelas@users.noreply.github.com>
2019-06-20 01:00:31 -0700

    Fixed CUDA/CPU drop_connect bug

--- efficientnet_pytorch/utils.py
+++ efficientnet_pytorch/utils.py
@@ -69,7 +69,7 @@ def drop_connect(inputs, p, training):
     batch_size = inputs.shape[0]
     keep_prob = 1 - p
     random_tensor = keep_prob
-    random_tensor += torch.rand([batch_size, 1, 1, 1], dtype=inputs.dtype)  # uniform [0,1)
+    random_tensor += torch.rand([batch_size, 1, 1, 1], dtype=inputs.dtype, device=inputs.device)
     binary_tensor = torch.floor(random_tensor)
     output = inputs / keep_prob * binary_tensor
     return output
