a11fc4c921882dd4ce07c5a7b337f5948b85a402
Luke <luke.melas@gmail.com>
2019-06-29 16:11:38 -0700

    Published new version to pip

--- efficientnet_pytorch/__init__.py
+++ efficientnet_pytorch/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "0.3.0"
 from .model import EfficientNet
 from .utils import (
     GlobalParams,
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/lukemelas/efficientnet_pytorch'
 EMAIL = 'lmelaskyriazi@college.harvard.edu'
 AUTHOR = 'Luke'
 REQUIRES_PYTHON = '>=3.5.0'
-VERSION = '0.2.0'
+VERSION = '0.3.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
@@ -109,16 +109,13 @@ setup(
     extras_require=EXTRAS,
     include_package_data=True,
     license='Apache',
-    # classifiers=[
-    #     # Trove classifiers
-    #     # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    #     'License :: OSI Approved :: MIT License',
-    #     'Programming Language :: Python',
-    #     'Programming Language :: Python :: 3',
-    #     'Programming Language :: Python :: 3.6',
-    #     'Programming Language :: Python :: Implementation :: CPython',
-    #     'Programming Language :: Python :: Implementation :: PyPy'
-    # ],
+    classifiers=[
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
     # $ setup.py publish support.
     cmdclass={
         'upload': UploadCommand,
