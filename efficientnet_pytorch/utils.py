985d0e84ac731049f62c7d89b37832d5083392a8
Luke <luke.melas@gmail.com>
2019-06-29 16:15:41 -0700

    Added identity to support older versions of pytorch

--- efficientnet_pytorch/utils.py
+++ efficientnet_pytorch/utils.py
@@ -119,12 +119,20 @@ class Conv2dStaticSamePadding(nn.Conv2d):
         if pad_h > 0 or pad_w > 0:
             self.static_padding = nn.ZeroPad2d((pad_w // 2, pad_w - pad_w // 2, pad_h // 2, pad_h - pad_h // 2))
         else:
-            self.static_padding = nn.Identity()
+            self.static_padding = Identity()
 
     def forward(self, x):
         x = self.static_padding(x)
         x = F.conv2d(x, self.weight, self.bias, self.stride, self.padding, self.dilation, self.groups)
         return x
+
+
+class Identity(nn.Module):
+    def __init__(self,):
+        super(Identity, self).__init__()
+
+    def forward(self, input):
+        return input
 
 
 ########################################################################
