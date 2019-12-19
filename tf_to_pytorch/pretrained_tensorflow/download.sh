bf2109cb7401567268dccd414629707263a7c789
Luke <luke.melas@gmail.com>
2019-12-19 07:35:48 +0000

    Improved developer experience

--- tf_to_pytorch/pretrained_tensorflow/download.sh
+++ tf_to_pytorch/pretrained_tensorflow/download.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 # This script accepts a single command-line argument, which specifies which model to download.
 # Only the b0, b1, b2, and b3 models have been released, so your command must be one of them.
 
@@ -9,6 +10,6 @@
 #   ./download.sh efficientnet-b3
 
 MODEL=$1
-wget https://storage.googleapis.com/cloud-tpu-checkpoints/efficientnet/ckptsaug/${MODEL}.tar.gz
+wget https://storage.googleapis.com/cloud-tpu-checkpoints/efficientnet/advprop/${MODEL}.tar.gz
 tar xvf ${MODEL}.tar.gz
 rm ${MODEL}.tar.gz
